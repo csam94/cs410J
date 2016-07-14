47376421db21e7dcb3214fd8dc1ad2070356b09c
Sam <csam@pdx.edu>
2016-07-13 22:10:34 -0700

    MAKE IT STOOOOOOOOOOOOOOOOOOOOOOOOP

--- koans/src/intermediate/AboutInnerClasses.java
+++ koans/src/intermediate/AboutInnerClasses.java
@@ -24,13 +24,13 @@ public class AboutInnerClasses {
     @Koan
     public void creatingInnerClassInstance() {
         Inner someObject = new Inner();
-        assertEquals(someObject.doStuff(), __);
+        assertEquals(someObject.doStuff(), "stuff");
     }
 
     @Koan
     public void creatingInnerClassInstanceWithOtherSyntax() {
         AboutInnerClasses.Inner someObject = this.new Inner();
-        assertEquals(someObject.doStuff(), __);
+        assertEquals(someObject.doStuff(), "stuff");
     }
 
     private int x = 10;
@@ -38,7 +38,7 @@ public class AboutInnerClasses {
     @Koan
     public void accessingOuterClassMembers() {
         Inner someObject = new Inner();
-        assertEquals(someObject.returnOuter(), __);
+        assertEquals(someObject.returnOuter(), x);
     }
 
     @Koan
@@ -48,7 +48,7 @@ public class AboutInnerClasses {
                 return 100;
             }
         }
-        assertEquals(new MethodInnerClass().oneHundred(), __);
+        assertEquals(new MethodInnerClass().oneHundred(), 100);
         // Where can you use this class?
     }
 
@@ -71,9 +71,9 @@ public class AboutInnerClasses {
     @Koan
     public void innerClassesInMethodsThatEscape() {
         AnotherInnerClass ic = new AnotherInnerClass();
-        assertEquals(ic.thousand(), __);
+        assertEquals(ic.thousand(), 1000);
         AnotherInnerClass theCrazyIC = ic.crazyReturn();
-        assertEquals(theCrazyIC.thousand(), __);
+        assertEquals(theCrazyIC.thousand(), 2000);
     }
 
     int theAnswer() {
@@ -87,7 +87,7 @@ public class AboutInnerClasses {
                 return 23;
             }
         };// <- Why do you need a semicolon here?
-        assertEquals(anonymous.theAnswer(), __);
+        assertEquals(anonymous.theAnswer(), 23);
     }
 
     @Koan
